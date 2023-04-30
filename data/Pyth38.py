ba0e87b7706869e438eb1a7208bafd057343245f
ruzlim <narut.lim@gmail.com>
2023-05-01 01:33:13 +0700

    sync

--- data/Pyth38.py
+++ data/Pyth38.py
@@ -5,7 +5,7 @@ parent_dir = os.getcwd()
 lib_dir = os.path.join(parent_dir, 'Lib38')
 sys.path.append(lib_dir)
 
-import pandas as pd
+# import pandas as pd
 # import numpy as np
 
 
@@ -15,6 +15,6 @@ print(f'\nlib_dir : {lib_dir}')
 print('\n')
 for i in sys.path:
     print(f'i : {i}')
-print(f'\npd : {pd}')
+# print(f'\npd : {pd}')
 # print(f'\nnp : {np}')
 print('\n')
